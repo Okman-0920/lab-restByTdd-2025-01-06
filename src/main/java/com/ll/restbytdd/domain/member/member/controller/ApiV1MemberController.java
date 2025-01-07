e43a37b2d37eca5e0c9a23ffbe67e2d71f86ae21
Okman-0920 <nohoksun94@gmail.com>
2025-01-07 11:32:06 +0900

    join 4th G

--- src/main/java/com/ll/restbytdd/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/restbytdd/domain/member/member/controller/ApiV1MemberController.java
@@ -1,8 +1,11 @@
 package com.ll.restbytdd.domain.member.member.controller;
 
+import com.ll.restbytdd.domain.member.member.entity.Member;
+import com.ll.restbytdd.domain.member.member.service.MemberService;
 import com.ll.restbytdd.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 public class ApiV1MemberController {
+    private final MemberService memberService;
+
+    record MemberJoinReqBody (
+            String username,
+            String password,
+            String nickname
+    ) {
+    }
 
     @PostMapping("/join")
-    public RsData<Void> join() {
+    public RsData<Void> join(
+            @RequestBody MemberJoinReqBody reqBody
+    ) {
+        Member member = memberService.join(reqBody.username, reqBody.password, reqBody.nickname);
 
-        return new RsData<>("201-1", "무명님 환영합니다");
+        return new RsData<>(
+                "201-1",
+                "%s 님 환영합니다".formatted(member.getName()));
     }
 }
