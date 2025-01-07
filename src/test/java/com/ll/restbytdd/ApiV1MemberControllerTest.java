af93beddfa50f39194482ed335d469d383158498
Okman-0920 <nohoksun94@gmail.com>
2025-01-07 10:22:01 +0900

    join, r

---
+++ src/test/java/com/ll/restbytdd/ApiV1MemberControllerTest.java
@@ -0,0 +1,39 @@
+package com.ll.restbytdd;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
+class ApiV1MemberControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("회원가입")
+    void t1() throws Exception {
+        // 회원가입 요청
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/member/1")
+                )
+                .andDo(print()); // 결과를 콘솔에 출력
+
+        // 201 Created 상태코드 검증
+        resultActions
+                .andExpect(status().isCreated());
+    }
+}
